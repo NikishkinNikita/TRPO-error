027dbbf81141af16edf93660e3ee6dc36d723c46
NikishkinNikita <nikishkinnikita@list.ru>
2020-11-04 21:30:49 +0300

    Released Lab2

--- A.php
+++ A.php
@@ -1,20 +1,37 @@
 <?php
+
 class A
 {
+	function line($a, $b){
+	    if($a==0){return null;}
+        return $this->x=-$b/$a;
+	}
+	protected $x;
 }
 class B extends A
 {
-    public function __construct($a, $b){
-    	$this->a = $a;
-    	$this->b = $b;
-    }
-    protected $a;
-    protected $b;
+	function square($a, $b, $c){
+	    if($a==0) return parent::line($b, $c);
+    	$d=$this->discriminant($a, $b, $c);
+		
+    	if($d==0) {
+			$d=sqrt($d);
+			return $this->x=array((-$b+$d)/(2*$a));
+    	}
+
+		if($d>0){
+			$d=sqrt($d);
+			return $this->x=array((-$b+$d)/(2*$a),(-$b-$d)/(2*$a));
+		}
+		return null;
+	}
+	protected function discriminant($a, $b, $c){
+	    return (($b*$b)-4*$a*$c);
+	}
 }
-$a1 = new A();
-$a2 = new A();
-$b4 = new B($a1, $a2);
-$a3 = new A();
-$b5 = new B($a3, $b4);
-var_dump($b5);
-?>
+$a1=new A();
+$b2=new B();
+$a1->line(0, -6);
+$b2->square(2, 8, 2);
+var_dump($a1);
+var_dump($b2);
